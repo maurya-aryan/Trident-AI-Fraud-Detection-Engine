0032d09531b0f72d7cca435f4ffa629aee3d0bc6
maurya-aryan <aryan5592ar@gmail.com>
2026-03-17 09:56:51 +0530

    Commiting the run files

---
+++ frontend-test/src/App.jsx
@@ -0,0 +1,16 @@
+import EmailFunnel from './EmailFunnel';
+
+function App() {
+  return (
+    <div style={{
+      margin: 0,
+      padding: 0,
+      minHeight: '100vh',
+      background: '#000'
+    }}>
+      <EmailFunnel />
+    </div>
+  );
+}
+
+export default App;
